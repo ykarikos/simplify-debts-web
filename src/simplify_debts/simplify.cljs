2ee459e3ee590c0cf5f2158886978e60c5ff0d33
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2018-04-02 13:24:17 +0300

    port weight-to-edge simplification algorith from python

--- src/simplify_debts/simplify.cljs
+++ src/simplify_debts/simplify.cljs
@@ -17,12 +17,48 @@
            to-key (+ old-to-weight weight)
            from-key (- old-from-weight weight))))
 
+(defn- find-greater-weight [weight weights]
+  (->> weights
+      (filter #(> (second %) weight))
+      first
+      first))
+
+(defn- find-target [current-weight weights nodes]
+  (let [target (find-greater-weight (Math/abs current-weight) weights)]
+    (if (and (< current-weight 0) (not (nil? target)))
+      target
+      (first nodes))))
+
+(defn- weight-to-edge [result node]
+  (let [weights (:weights result)
+        sorted-weights (:sorted-weights result)
+        rest-nodes (:rest-nodes result)
+        edges (:edges result)
+        current-weight (node weights)
+        transact (Math/abs current-weight)]
+    (if (= current-weight 0)
+      result
+      (let [target (find-target current-weight sorted-weights rest-nodes)]
+        {:weights (assoc weights target (+ (target weights) current-weight))
+         :sorted-weights sorted-weights
+         :rest-nodes (rest rest-nodes)
+         :edges (conj edges {:from node :to target :amount transact})}))))
 
 
-; (reduce add-weight {} edges
-
-(defn edges-to-weights [edges]
+(defn- edges-to-weights [edges]
   (->> edges
        (reduce add-weight {})
        (filter #(not= 0 (val %)))
        (sort #(compare (val %1) (val %2)))))
+
+(defn simplify [edges]
+  (let [sorted-weights (edges-to-weights edges)
+        weights-map (into (sorted-map) sorted-weights)
+        nodes (keys sorted-weights)
+        start-nodes (take (- (count nodes) 1) nodes)]
+    (reduce weight-to-edge
+            {:weights weights-map
+             :rest-nodes (rest nodes)
+             :edges []
+             :sorted-weights sorted-weights}
+            start-nodes)))
