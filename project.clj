f1fc44c49fc5e23625d307ad1efe2fe5d34d5b00
Yrjö Kari-Koskinen <ykk@peruna.fi>
2018-04-14 23:17:16 +0300

    move public to docs

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject simplify-debts "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+  :license {:name "Eclipse docs License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.9.0"]
@@ -20,20 +20,20 @@
    [:cljsbuild :builds :app :compiler :output-dir]
    [:cljsbuild :builds :app :compiler :output-to]]
 
-  :resource-paths ["public"]
+  :resource-paths ["docs"]
 
   :figwheel {:http-server-root "."
              :nrepl-port 7002
              :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]
-             :css-dirs ["public/css"]}
+             :css-dirs ["docs/css"]}
 
   :cljsbuild {:test-commands {"test" ["lein" "doo" "phantom" "test" "auto"]}
               :builds {:app
                        {:source-paths ["src" "env/dev/cljs"]
                         :compiler
                         {:main "simplify-debts.dev"
-                         :output-to "public/js/app.js"
-                         :output-dir "public/js/out"
+                         :output-to "docs/js/app.js"
+                         :output-dir "docs/js/out"
                          :asset-path   "js/out"
                          :source-map true
                          :optimizations :none
@@ -44,16 +44,16 @@
                        :release
                        {:source-paths ["src" "env/prod/cljs"]
                         :compiler
-                        {:output-to "public/js/app.js"
-                         :output-dir "public/js/release"
+                        {:output-to "docs/js/app.js"
+                         :output-dir "docs/js/release"
                          :asset-path   "js/out"
                          :optimizations :advanced
                          :pretty-print false}}
                        :test
-                        {:source-paths ["src" "test"]
-                         :compiler {:main runners.doo
-                                    :optimizations :none
-                                    :output-to "resources/public/cljs/tests/all-tests.js"}}}}
+                       {:source-paths ["src" "test"]
+                        :compiler {:main runners.doo
+                                   :optimizations :none
+                                   :output-to "resources/docs/cljs/tests/all-tests.js"}}}}
 
   :aliases {"package" ["do" "clean" ["cljsbuild" "once" "release"]]}
 
